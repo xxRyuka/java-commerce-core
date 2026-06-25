cdce815ca308c90e0aa29cb059030268386463b1
xxRyuka <enesfurkankus16@gmail.com>
2026-06-25 03:08:02 +0300

    Definition of Done
    [ ] cart package oluşturuldu.
    
    [ ] CartItem class oluşturuldu.
    
    [ ] CartItem içinde Product ve quantity var.
    
    [ ] CartItem.getLineTotal() BigDecimal ile hesaplanıyor.
    
    [ ] Cart class oluşturuldu.
    
    [ ] Cart içinde List<CartItem> var.
    
    [ ] Cart.addProduct(...) çalışıyor.
    
    [ ] Aynı ürün ikinci kez eklenince yeni CartItem oluşmuyor.
    
    [ ] Aynı ürünün quantity değeri artıyor.
    
    [ ] Cart.removeProductById(...) true/false dönüyor.
    
    [ ] Cart.changeProductQuantity(...) true/false dönüyor.
    
    [ ] Cart.getTotalAmount() doğru toplamı hesaplıyor.
    
    [ ] Cart.getTotalQuantity() doğru toplam adedi hesaplıyor.
    
    [ ] Cart.isEmpty() çalışıyor.
    
    [ ] Main içinde printCart(Cart cart) helper methodu var.
    
    [ ] Main'de aynı ürün tekrar ekleme testi var.
    
    [ ] Main'de miktar değiştirme testi var.
    
    [ ] Main'de ürün silme testi var.
    
    [ ] Main'de olmayan ürün silme testi var.
    
    [ ] Sepete ekleme sırasında Product stockQuantity değişmiyor.

---
+++ src/main/java/com/coreJava/commerce/card/CardItem.java
@@ -0,0 +1,38 @@
+package com.coreJava.commerce.card;
+
+import com.coreJava.commerce.catalog.Product;
+
+import java.math.BigDecimal;
+
+public class CardItem {
+    private Product item;
+    private int quantity;
+
+    public CardItem(Product item, int quantity) {
+        this.item = item;
+        this.quantity = quantity;
+    }
+
+    public Product getItem() {
+        return item;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+
+    public void increaseQuantity(int additionalQuantity) {
+        this.quantity += additionalQuantity;
+    }
+
+
+    public void changeQuantity(int newQuantity) {
+        this.quantity = newQuantity;
+    }
+
+    public BigDecimal getLineTotal() {
+        BigDecimal quantityBigDecimal = BigDecimal.valueOf(quantity);
+        return quantityBigDecimal.multiply(item.getPrice());
+    }
+}
